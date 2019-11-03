d5396c04fd746e78692e12a14dddd1b65a70e814
Matt DeMonaco <mdemonaco@gmail.com>
2019-11-03 00:25:09 -0700

    formatted user input forms correcty, implemented Ajax call along with variable query parameters

--- app.js
+++ app.js
@@ -1,5 +1,36 @@
+var queryURL = api + apiKey + apiSearch;
+var api = "https://api.giphy.com/v1/gifs/search?";
+var apiKey = "api_key=zLtOADNeg8ECuUXQJAL5Uubqd8ZRCfs8";
+var apiSearch = "&q=disney&limit=10&offset=0&rating=G&lang=en";
+// var search = ""
+// var url = "";
+var q = "";
+
+
 $(document).ready(function(){
-$(".button").click(function(){
-alert("hihihi");
-})
-})
+$(".button").click(function clicked(){
+    event.preventDefault();
+    alert("hihihi");
+ 
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(resonse){
+        console.log(response);
+    
+    for (var i = 0; i < data.length; i++)
+   var imageUrl = (data.data[1].images.original.url);
+  var createImg = $("<img>");
+  createImg.attr("src", imageUrl);
+  $(document).prepend(createImg);
+
+
+console.log(gifs[i].images.original.url);
+console.log(response)
+});   
+});
+});
+
+
+
+
