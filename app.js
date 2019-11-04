2e7677d6a48a627a17161dd04fb929f79f390f19
Matt DeMonaco <mdemonaco@gmail.com>
2019-11-03 17:11:02 -0800

    buttons now append to page and Ajax call is being returned

--- app.js
+++ app.js
@@ -1,83 +1,67 @@
 var characters = ["Aladdin", "Ariel", "Cinderella", "Sleeping Beauty", "Nemo", "Mickey Mouse", "Snow White", "Flounder", "Ursula", "Mulan"];
-
-// var queryURL = api +  apiKey + searchCharacter + apiSearch;
-// var api = "https://api.giphy.com/v1/gifs/search?";
-// var apiKey = "api_key=zLtOADNeg8ECuUXQJAL5Uubqd8ZRCfs8&q=";
-// var searchCharacter = $("#search-gifs").val().trim().toString();
-// var apiSearch = "&limit=10&offset=0&rating=G&lang=en";
-// var search = ""
-// var url = "";
-// var q = "";
+// displayArray();
 
 $(document).ready(function () {
-    $("#add-gif").click(function clicked() {
-        // event.preventDefault();
-        var gifQuery = $("#search-gifs").val()
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifQuery + "&api_key=zLtOADNeg8ECuUXQJAL5Uubqd8ZRCfs8&limit=10&offset=0&rating=G&lang=en"
+   function renderButtons() {
+        $("#displayGif").empty();
+        for (var i = 0; i < characters.length; i++) {
+            var newGif = $("<button>");
+            newGif.addClass("character")
+            newGif.attr("data-name", characters[i]);
+            newGif.text(characters[i]);
+            $("#displayGif").append(newGif);
 
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-
-            //going through the array to grab the URL from the JSON object and displaying them on the page
-            for (var i = 0; i < response.data.length; i++) {
-                //console.log(response.data);
-
-                var imageUrl = response.data[i].images.fixed_height_small_still.url;
-                console.log(imageUrl)
-                var createImg = $("<img>");
-                createImg.attr("src", imageUrl);
-                $("#displayGif").append(createImg);
-                //console.log(data[i].images.original.url);
-                //console.log(response)
-            }
-           addButton(gifQuery)
-        })
-        //adds buttons to the page
-        function addButton(newTerm) {
-            console.log(newTerm)
-            characters.push(newTerm);
-            $("#add-gif").empty();
-            console.log("below are charactors")
-            console.log(characters)
-            for (var i = 0; i < characters.length; i++)
-            
-                var newButton = $("<button>");
-            newButton.addClass("Disney");
-            newButton.attr("character-name", characters[i]);
-            newButton.text(characters[i]);
-            $("#add-gif").append(newButton);
         }
 
-        $("#add-gif").on("click", function () {
-            event.preventDefault();
-            var searchTerm = $("#search-gifs").val().trim();
-            characters.push(searchTerm);
+    }
+$("#add-gif").on("click", function(event){
+event.preventDefault();
+var userInput = $("#search-gifs").val().trim();
+characters.push(userInput);
+var gifQuery = $(this).attr("data-name");
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifQuery + "&api_key=zLtOADNeg8ECuUXQJAL5Uubqd8ZRCfs8&limit=10&offset=0&rating=G&lang=en";
+console.log(gifQuery);
+renderButtons();
 
-            addButton();
-        });
-
-        
-
-        $("img").on("click", function () {
-            alert("ypour in an onclick")
-            var state = $(this).attr(data - state);
-            if (state === "still") {
-                $(this).attr("src", $(this).attr("data-animate"));
-                $(this).attr("data-state", "animate");
-            } else {
-                $(this).attr("src", $(this).attr("data-still"));
-                $(this).attr("data-state", "still");
-
-            }
-        });
-
-    });
-});
-
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response) {
+    console.log(queryURL);
+console.log(response.data)
+})
+    //going through the array to grab the URL from the JSON object and displaying them on the page
+    // for (var i = 0; i < response.data.length; i++) {
+    //     console.log(response.data);
+        // var imageUrl = response.data[i].images.fixed_height_small_still.url;
+        // console.log(imageUrl)
+        // var createImg = $("<img>");
+        // createImg.attr("src", imageUrl);
+        // $("#displayGif").append(createImg);
+        //console.log(data[i].images.original.url);
+        //console.log(response)
+    
+    //going through the array to grab the URL from the JSON object and displaying them on the page
 
 
 
+renderButtons();
+})
+
+renderButtons();
+})
 
 
+
+// var queryURL = "https://api.giphy.com/v1/gifs/search?q=simba&api_key=zLtOADNeg8ECuUXQJAL5Uubqd8ZRCfs8&limit=10&offset=0&rating=G&lang=en"
+
+// $.ajax({
+//     url: queryURL,
+//     method: "GET"
+// }).then(function (response) {
+//     console.log(response.data)
+//     //going through the array to grab the URL from the JSON object and displaying them on the page
+//     for (var i = 0; i < response.data.length; i++) {
+//         console.log(response.data);
+//         var imageUrl = response.data[i].images.fixed_height_small_still.url;
+//         console.log(imageUrl)
