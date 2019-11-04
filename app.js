7120b9d46fef0ccc1da634ad1f353481d9b75942
Matt DeMonaco <mdemonaco@gmail.com>
2019-11-03 21:06:38 -0800

    gifs now show ten when button is clicked. went into the array with index i to do this.

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
 var characters = ["Aladdin", "Ariel", "Cinderella", "Sleeping Beauty", "Nemo", "Mickey Mouse", "Snow White", "Flounder", "Ursula", "Mulan"];
 // displayArray();
+// $(document).on("click", ".characters", call);
 
 $(document).ready(function () {
+    function displayGif() {
+        var gifQuery = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifQuery + "&api_key=zLtOADNeg8ECuUXQJAL5Uubqd8ZRCfs8&limit=10&offset=0&rating=G&lang=en";
+        console.log(gifQuery);
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(queryURL);
+            console.log(response.data);
+            for (var i = 0; i < response.data.length; i++) {
+                console.log(response.data[i].url);
+                var imageUrl = (response.data[i].images.fixed_width_small_still.url);
+                console.log(imageUrl)
+                var createImg = $("<img>");
+                createImg.attr("data-name");
+                createImg.attr("src", imageUrl);
+                $("#displayGif").append(createImg);
+                console.log(response.data[i].url);
+                console.log(response);
+                // $("#displayGif").append(response.data.images.fixed_height_small_still.url);
+            }
+        })
+    }
     function renderButtons() {
         $("#displayGif").empty();
         for (var i = 0; i < characters.length; i++) {
@@ -12,39 +37,14 @@ $(document).ready(function () {
             $("#displayGif").append(newGif);
 
         }
-
     }
     $("#add-gif").on("click", function (event) {
         event.preventDefault();
         var userInput = $("#search-gifs").val().trim();
         characters.push(userInput);
-        var gifQuery = userInput
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifQuery + "&api_key=zLtOADNeg8ECuUXQJAL5Uubqd8ZRCfs8&limit=10&offset=0&rating=G&lang=en";
-        console.log(gifQuery);
+        console.log(characters);
         renderButtons();
-
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-            console.log(queryURL);
-            console.log(response.data)
-             //going through the array to grab the URL from the JSON object and displaying them on the page
-            for (var i = 0; i < response.data.length; i++) {
-                    console.log(response.data);  
-                    var imageUrl = response.data[i].images.fixed_height_small_still.url;
-                    console.log(imageUrl)
-                    var createImg = $("<img>");
-                    createImg.attr("src", imageUrl);
-                    $("#displayGif").append(createImg);
-                
-                    //console.log(data[i].images.original.url);
-                    //console.log(response)
-
-            }
-                })
-                renderButtons();
-        })
-
-            renderButtons();
-        })
+    })
+    $(document).on("click", ".character", displayGif);
+    renderButtons();
+})
